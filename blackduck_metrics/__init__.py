f4051b369b5bd9166f086068ba09922af34d66f9
Jouni Lehto <lehto.jouni@gmail.com>
2026-02-16 12:33:05 +0200

    BUpdate license format in pyproject.toml

--- blackduck_metrics/__init__.py
+++ blackduck_metrics/__init__.py
@@ -5,7 +5,7 @@ A tool for analyzing Black Duck scan metrics from CSV files in zip archives.
 Generates interactive HTML dashboards with time series analysis and filtering.
 """
 
-__version__ = "0.1.11"
+__version__ = "0.1.12"
 __author__ = "Jouni Lehto"
 __email__ = "lehto.jouni@gmail.com"
 
